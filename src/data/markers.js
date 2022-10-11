9e16f0cd67de07c6df659494ffa17519d89ab278
German <72597058+GM-GitH@users.noreply.github.com>
2022-10-12 01:50:31 +0200

    Adds Hotel Locations

--- src/data/markers.js
+++ src/data/markers.js
@@ -2,106 +2,106 @@ export const locations = [
   {
     lat: 36.709119,
     lng: -4.422833,
-    com: "Andalucia"
+    com: "Andalucia",
   },
   {
     lat: 36.680489,
     lng: -4.447209,
-    com: "Andalucia"
+    com: "Andalucia",
   },
   {
     lat: 37.390263,
     lng: -5.874415,
-    com: "Andalucia"
+    com: "Andalucia",
   },
   {
     lat: 41.56144,
     lng: -0.835124,
-    com: "Aragon"
+    com: "Aragon",
   },
   {
     lat: 43.518953,
     lng: -5.697258,
-    com: "Asturias"
+    com: "Asturias",
   },
   {
     lat: 38.904361,
     lng: 1.419452,
-    com: "Baleares"
+    com: "Baleares",
   },
   {
     lat: 28.112115,
     lng: -15.421376,
-    com: "Canarias"
+    com: "Canarias",
   },
   {
     lat: 43.444384,
     lng: -3.841548,
-    com: "Cantabria"
+    com: "Cantabria",
   },
   {
     lat: 41.642278,
     lng: -4.817843,
-    com: "Castilla y Leon"
+    com: "Castilla y Leon",
   },
   {
     lat: 39.007614,
     lng: -1.965695,
-    com: "Castilla - La Mancha"
+    com: "Castilla - La Mancha",
   },
   {
     lat: 41.370139,
     lng: 2.151817,
-    com: "Cataluña"
+    com: "Cataluña",
   },
   {
     lat: 39.473328,
     lng: -0.403895,
-    com: "Valencia"
+    com: "Valencia",
   },
   {
     lat: 39.454901,
     lng: -6.364369,
-    com: "Extremadura"
+    com: "Extremadura",
   },
   {
     lat: 42.846839,
     lng: -8.540115,
-    com: "Galicia"
+    com: "Galicia",
   },
   {
     lat: 40.406843,
     lng: -3.678032,
-    com: "Madrid"
+    com: "Madrid",
   },
   {
     lat: 37.963547,
     lng: -1.240913,
-    com: "Murcia"
+    com: "Murcia",
   },
   {
     lat: 42.796727,
     lng: -1.640055,
-    com: "Navarra"
+    com: "Navarra",
   },
   {
     lat: 43.049749,
     lng: -2.471342,
-    com: "Pais Vasco"
+    com: "Pais Vasco",
   },
   {
     lat: 42.447416,
     lng: -2.442706,
-    com: "La Rioja"
+    com: "La Rioja",
   },
   {
     lat: 35.888078,
     lng: -5.323505,
-    com: "Ceuta"
+    com: "Ceuta",
   },
   {
     lat: 35.291816,
     lng: -2.93766,
-    com: "Melilla"
+    com: "Melilla",
   }
 ]
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -31,14 +31,18 @@ let watchID;
 let geoLoc;
 
 const markLatLng = { lat: 40.416668, lng: -3.704064 };
+
 function initMap() {
+
+  const bounds = new google.maps.LatLngBounds();
+  const markersArray = [];
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 6,
     center: markLatLng,
   });
-  const infoWindow = new google.maps.InfoWindow({
+  let infoWindow = new google.maps.InfoWindow({
     content: "",
-    disableAutoPan: true,
+    disableAutoPan: false,
   });
   const markers = locations.map((position) => {
     const marker = new google.maps.Marker({
@@ -47,18 +51,87 @@ function initMap() {
     });
     // https://developers.google.com/maps/documentation/javascript/marker-clustering
     marker.addListener("click", () => {
-      infoWindow.setContent(label);
+      infoWindow.setContent(`Miranda Hotel: ${position.com}`);
       infoWindow.open(map, marker);
     });
     return marker;
   });
   new markerClusterer.MarkerClusterer({ markers, map });
+  infoWindow = new google.maps.InfoWindow();
+
+  const locationButton = document.createElement("button");
+
+  locationButton.textContent = "📍";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.RIGHT_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("You are here");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+
+
+
+  //******************************************************************* initialize MATRIX services
+  const geocoder = new google.maps.Geocoder();
+  const service = new google.maps.DistanceMatrixService();
+
+  // build request
+  const origin1 = "Madrid, Spain";
+  const request = {
+    origins: [origin1],
+    destinations: locations,
+    travelMode: google.maps.TravelMode.DRIVING,
+    unitSystem: google.maps.UnitSystem.METRIC,
+    avoidHighways: false,
+    avoidTolls: false,
+  };
+
+  // put request on page
+  document.getElementById("request").innerText = JSON.stringify(request, null, 2);
+  // get distance matrix response
+  service.getDistanceMatrix(request).then((response) => {
+    // put response
+    document.getElementById("response").innerText = JSON.stringify(response, null, 2);
+    // show on map
+    const originList = response.originAddresses;
+    const destinationList = response.destinationAddresses;
+
+    // deleteMarkers(markersArray);
+
+  });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+
 }
 
 
-
-
 window.initMap = initMap;
-
 
 //------------------------------------------------------ NEW SCRIPT
